29cba55a4b11322d93584e0f0852525989f6e1be
xulianpeng <xulianpengxlp@126.com>
2017-01-12 14:52:14 +0800

    解决一些视频相关的坑点
    坑点不少

--- SwiftTest/videoRootViewController.swift
+++ SwiftTest/videoRootViewController.swift
@@ -19,14 +19,18 @@ class videoRootViewController: XLPBaseViewController {
     let videoBt4 = UIButton.init(type: UIButtonType.custom)
     
     
+    
+    var playerVC1 = MPMoviePlayerController()
+    
     var avPlayer = AVPlayer()
+    var avPlayerLayer = AVPlayerLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         layoutViews()
         
     }
-    
+    //悠军毅
     func layoutViews()  {
         
         
@@ -52,18 +56,29 @@ class videoRootViewController: XLPBaseViewController {
             make.height.equalTo(videoBt1.snp.height)
         }) { (bt) in
             
+            // 坑点之一 playerVC1 必须是全局的 变量  否则 不会播放
+            self.playerVC1 = MPMoviePlayerController.init(contentURL: self.url! as URL!)
+            self.playerVC1.view.frame = CGRect(x:0,y:300,width:SCREENWIDTH,height:SCREENWIDTH * 9 / 16)
             
-            let playerVC = MPMoviePlayerController.init(contentURL: self.url! as URL!)
-            playerVC?.view.frame = CGRect(x:0,y:300,width:SCREENWIDTH,height:SCREENWIDTH * 9 / 16)
-            self.view.addSubview((playerVC?.view)!)
+            self.view.addSubview((self.playerVC1.view)!)
             
-            playerVC?.controlStyle = .default
-            playerVC?.scalingMode = .aspectFit
-            playerVC?.shouldAutoplay = true
-            playerVC?.prepareToPlay()
+            self.playerVC1.controlStyle = .default//embedded,fullscreen,none,default
+            //显示播放 按钮  快进 快退 全屏等 
+            //fullscreen  分上下两部分 上面是 完成按钮  进度条  下面是 快退 播放 快进 三个按钮 ,其中 点完成时 播放暂停 , 点快进或快退时,若无视频队列 则会黑屏 
+            //none时 相关的控制按钮 进度条 全部没有
+            //default 同 embedded
+            //embedded 播放按钮  进度条 还有个类似全屏的按钮 但是 一点击 先放大但画面消失 声音还在
+            
+            self.playerVC1.scalingMode = .none
+            
+            //aspectFit  成比例缩放 上下部分 会有部分黑屏
+            //aspectFill / fill/none  全屏填充
+            self.playerVC1.shouldAutoplay = true
+            self.playerVC1.repeatMode = .one
+            self.playerVC1.prepareToPlay()
            
-            print("===============\(playerVC?.readyForDisplay)")
-            playerVC?.play()
+            print("===============\(self.playerVC1.readyForDisplay)")
+            self.playerVC1.play()
         }
         
         videoBt3.xlpInitEassyButton("AVPlayerViewController", titleColor: .blue, fontSize: 14, backgroundColor: XLPRandomColor(), cornerRedius: 4, superView: view, snpMaker: { (make) in
@@ -95,11 +110,11 @@ class videoRootViewController: XLPBaseViewController {
             let avPlayerItem = AVPlayerItem.init(url: self.url! as URL)
             self.avPlayer = AVPlayer.init(playerItem: avPlayerItem)
             self.avPlayer.play()
-            let avPlayerLayer = AVPlayerLayer.init(player: self.avPlayer)
-            avPlayerLayer.frame = CGRect(x:0,y:300,width:SCREENWIDTH,height:SCREENWIDTH * 9 / 16)
-            avPlayerLayer.borderColor = UIColor.red.cgColor
-            avPlayerLayer.borderWidth = 3
-            self.view.layer.addSublayer(avPlayerLayer)
+            self.avPlayerLayer = AVPlayerLayer.init(player: self.avPlayer)
+            self.avPlayerLayer.frame = CGRect(x:0,y:300,width:SCREENWIDTH,height:SCREENWIDTH * 9 / 16)
+            self.avPlayerLayer.borderColor = UIColor.red.cgColor
+            self.avPlayerLayer.borderWidth = 3
+            self.view.layer.addSublayer(self.avPlayerLayer)
             self.avPlayer.volume  = 0.3
             
         }
@@ -109,12 +124,26 @@ class videoRootViewController: XLPBaseViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         
-        self.view = nil //针对AVPlayer 若在退出播放器页面时,若未将放置该AVPlayer的view置为nil,则其内存不会被释放,会出现多个声音来源
-        //下面这两个方法 貌似释放 AVPlayer无效
-        avPlayer.currentItem?.cancelPendingSeeks()
-        avPlayer.currentItem?.asset.cancelLoading()
+//        self.view = nil //针对AVPlayer 若在退出播放器页面时,若未将放置该AVPlayer的view置为nil,则其内存不会被释放,会出现多个声音来源
+//        //下面这两个方法 貌似释放 AVPlayer无效
+//        avPlayer.currentItem?.cancelPendingSeeks()
+//        avPlayer.currentItem?.asset.cancelLoading()
         
         //以上两种 方法 貌似均无效
+        
+        /*********************/
+        //这个方法 针对 第二种方法 内存有所减少,但还是有所增加,多次打开视频可能会有视频暴增的危险 但 不如第一种和第三种(这俩是退出后内存完全不增加)
+        self.playerVC1.stop()
+        self.playerVC1.view.removeFromSuperview()
+        //*****************//
+        
+        
+        //这个根本就没释放
+        self.avPlayer.pause()
+        self.avPlayerLayer.removeFromSuperlayer()
+        self.view = nil
+        
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
