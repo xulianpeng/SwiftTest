2a94074b00a9045bcea9e2cdbd7abfebf1882f43
xulianpeng <xulianpengxlp@126.com>
2017-02-24 18:21:32 +0800

    添加电话号码正则判断
    添加电话号码正则判断

--- SwiftTest/ConstFile.swift
+++ SwiftTest/ConstFile.swift
@@ -165,7 +165,30 @@ func showHud(text:String,yHeight:CGFloat) -> Void {
     
 //    xlpHud.init(frame: <#T##CGRect#>, text: <#T##String#>)
 }
-
+//MARK: 正则判断是否是电话号码
+func isTelNumber(_ num:String)->Bool{
+    
+    let mobile = "^1(3[0-9]|4[57]|5[0-35-9]|7[0678]|8[0-9])\\d{8}$"
+    let  CM = "(^1(3[4-9]|4[7]|5[0-27-9]|7[8]|8[2-478])\\d{8}$)|(^1705\\d{7}$)";
+    let  CU = "(^1(3[0-2]|4[5]|5[56]|7[6]|8[56])\\d{8}$)|(^1709\\d{7}$)";
+    let  CT = "(^1(33|53|77|8[019])\\d{8}$)|(^1700\\d{7}$)";
+    let regextestmobile = NSPredicate(format: "SELF MATCHES %@",mobile)
+    let regextestcm = NSPredicate(format: "SELF MATCHES %@",CM )
+    let regextestcu = NSPredicate(format: "SELF MATCHES %@" ,CU)
+    let regextestct = NSPredicate(format: "SELF MATCHES %@" ,CT)
+    
+    if ((regextestmobile.evaluate(with: num) == true)
+        || (regextestcm.evaluate(with: num)  == true)
+        || (regextestct.evaluate(with: num) == true)
+        || (regextestcu.evaluate(with: num) == true))
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
 //#if DEBUG
 //#else
 //#endif
